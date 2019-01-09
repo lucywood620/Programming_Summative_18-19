7f2a8933c205130940814965856f08379c77da9d
Lucy <lucywood620@gmail.com>
2019-01-09 21:10:57 +0000

    adding a text label to the colour slider

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var col;
 let circle1;
 let circle2;
 let circle3;
-let slider;
+let colour_slider;
 
 class Circle{
   constructor(x,y,i) {
@@ -27,8 +27,8 @@ class Circle{
   }
 
   show() {
-    stroke(slider.value(), 255, 255);
-    fill(slider.value(), 255, 255, 127);
+    stroke(colour_slider.value(), 255, 255);
+    fill(colour_slider.value(), 255, 255, 127);
     ellipse(this.x,this.y,this.w,this.w);
   }
 
@@ -39,9 +39,9 @@ function setup(){
   // hue, saturation, and brightness
   colorMode(HSB, 255);
   // slider has a range between 0 and 255 with a starting value of 0
-  slider = createSlider(0, 255, 0);
+  colour_slider = createSlider(0, 255, 0);
   //positioning the slider in the top left hand corner
-  slider.position(20,20);
+  colour_slider.position(20,20);
 
   //creating 3 circle objects
   circle1 = new Circle(100,0,0);
@@ -51,6 +51,11 @@ function setup(){
 
 function draw(){
   background(242);
+  //adding text labels to the sliders
+  fill(0);
+  stroke(0)
+  text("Circle colour",35,45);
+  textSize(15);
 
   circle1.move();
   circle1.show();
