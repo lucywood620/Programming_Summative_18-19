9fac6e91d7680a041779996c4e64886a24bf7de2
Lucy <lucywood620@gmail.com>
2019-01-16 20:45:52 +0000

    changing the sliders to be HTML elements instead of javascript code

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var time = 0;
 let circle1;
 let circle2;
 let circle3;
-let circle4;
-let colour_slider;
-let size_slider;
 let speed_slider;
 let circles = []
+var colourSlider;
+var sizeSlider;
+var speedSlider;
 
 
 class Circle{
@@ -24,20 +24,20 @@ class Circle{
   
   move(){
     if (this.i < 360){
-      this.i = this.i + 0.5
+      this.i = this.i + 1;
     }
     else{
       this.i = 0
     }    
-    this.x = cos(radians(this.i)) * size_slider.value() + windowWidth / 2;
-    this.y = sin(radians(this.i)) * (size_slider.value()*2) + windowHeight / 2;
-    this.w = (sin(radians(time + this.i)) * (size_slider.value()*4));
+    this.x = cos(radians(this.i)) * sizeSlider + windowWidth / 2;
+    this.y = sin(radians(this.i)) * (sizeSlider*2) + windowHeight / 2;
+    this.w = (sin(radians(time + this.i)) * (sizeSlider*4));
     this.w = abs(this.w);
   }
 
-  show() {
-    stroke(colour_slider.value(), 255, this.brightness);
-    fill(colour_slider.value(), 255, this.brightness, 127);
+  show(colourSlider) {
+    stroke(colourSlider, 255, this.brightness);
+    fill(colourSlider, 255, this.brightness, 127);
     ellipse(this.x,this.y,this.w,this.w);
   }
 
@@ -66,7 +66,7 @@ class Circle{
     //credit: https://www.youtube.com/watch?v=Kp070rI_G48
     this.x = this.x + (this.circledirectionx * this.circlespeed);
     this.y = this.y + (this.circledirectiony * this.circlespeed);
-    this.w = size_slider.value()*4;
+    this.w = sizeSlider*4;
     this.w = abs(this.w);
 
     if (this.x < 0) { //off the left of the screen
@@ -87,15 +87,6 @@ class Circle{
 function setup(){
   createCanvas(windowWidth, windowHeight);
   colorMode(HSB, 255);
-
-  // creating a slider to change the colour of the circles
-  colour_slider = createSlider(0, 255, 0);
-  colour_slider.position(20,20);
-
-  //creating a slider to change the size of the circles
-  size_slider = createSlider(0,100,50);
-  size_slider.position(180,20);
-
   //creating 3 circle objects and adding them to an array
   circle1 = new Circle(100,0,0);
   circles.push(circle1);
@@ -103,37 +94,27 @@ function setup(){
   circles.push(circle2);
   circle3 = new Circle(100,0,200);
   circles.push(circle3);
-  circle4 = new Circle(10,0,10);
-
 }
 
-//slider for speed
 //reset button
 //move starting position of sliders and balls
-//make an instruction side page about how to use elements
-//make sliders html elements
-
-//credit for sliders: https://www.w3schools.com/howto/howto_js_rangeslider.asp
 
 function draw(){
   background(242);
-  //adding text labels to the sliders
-  fill(0);
-  stroke(0)
-  text("circle colour",35,45);
-  text("circle size",203,45)
-  textSize(15);
+  colourSlider = document.getElementById('colour').value;
+  sizeSlider = document.getElementById("size").value;
+  speedSlider = document.getElementById("speed").value;
 
   for (let i = 0; i < circles.length; i++) {
     circles[i].hover(mouseX,mouseY);
-    circles[i].show();
-    //if the circle has been clicked and is meant to be bouncing then run the bounce function
+    circles[i].show(colourSlider);
     if (circles[i].bouncing == true) {
       circles[i].bounce()
     }
-    //otherwise the circle will continue to move in a circle as normal
     else {
       circles[i].move();
+    //if the circle has been clicked and is meant to be bouncing then run the bounce 
+    //function, otherwise the circle will continue to move in a circle as normal
     }
   }  
   time = time + 0.5
